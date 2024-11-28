947a4868ea1608d4c1d0a9a5ba0a14d9a44b92f0
zhangchao <zackability@hotmail.com>
2024-11-28 09:06:30 +0800

    fix: sysinternals-suite missing ctrl2cap error when install

--- .dev/gen_gui_shortcuts.ps1
+++ .dev/gen_gui_shortcuts.ps1
@@ -1,6 +1,6 @@
-$app_path = "C:\Users\zhangchao\Downloads\sysinternals-suite-20240620221100"
-$app_path_arm64 = "C:\Users\zhangchao\Downloads\sysinternals-suite-20240620221100-ARM64"
-$app_path_nano = "C:\Users\zhangchao\Downloads\sysinternals-suite-20240620221100-Nano"
+$app_path = "C:\Users\zhangchao\Downloads\sysinternals-suite-20240723000000"
+$app_path_arm64 = "C:\Users\zhangchao\Downloads\sysinternals-suite-20240723000000-ARM64"
+$app_path_nano = "C:\Users\zhangchao\Downloads\sysinternals-suite-20240723000000-Nano"
 
 mkdir temp
 
@@ -19,14 +19,14 @@ Get-ChildItem $app_path
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?=.exe)' }
 | Where-Object { IfGui -path $_.FullName }
 | ForEach-Object { $result.Add(@($_.Name, [System.IO.Path]::GetFileNameWithoutExtension($_.Name))) | Out-Null }
-$result | ConvertTo-Json > temp/ss_64_sc.json
+ConvertTo-Json $result > temp/ss_64_sc.json
 
 # 64 bit bin
 [System.Collections.ArrayList]$result = New-Object -TypeName System.Collections.ArrayList
 Get-ChildItem $app_path
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?=.exe)' }
 | ForEach-Object { $result.Add(@($_.Name, [System.IO.Path]::GetFileNameWithoutExtension($_.Name))) | Out-Null }
-$result | ConvertTo-Json > temp/ss_64_bin.json
+ConvertTo-Json $result > temp/ss_64_bin.json
 
 # 32 bit shortcut
 [System.Collections.ArrayList]$result = New-Object -TypeName System.Collections.ArrayList
@@ -35,7 +35,7 @@ Get-ChildItem $app_path
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?<!64.exe)$' }
 | Where-Object { IfGui -path $_.FullName }
 | ForEach-Object { $result.Add(@($_.Name, [System.IO.Path]::GetFileNameWithoutExtension($_.Name))) | Out-Null }
-$result | ConvertTo-Json > temp/ss_86_sc.json
+ConvertTo-Json $result > temp/ss_86_sc.json
 
 # 32 bit bin
 [System.Collections.ArrayList]$result = New-Object -TypeName System.Collections.ArrayList
@@ -43,7 +43,7 @@ Get-ChildItem $app_path
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?=.exe)' }
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?<!64.exe)$' }
 | ForEach-Object { $result.Add(@($_.Name, [System.IO.Path]::GetFileNameWithoutExtension($_.Name))) | Out-Null }
-$result | ConvertTo-Json > temp/ss_86_bin.json
+ConvertTo-Json $result > temp/ss_86_bin.json
 
 # arm64 shortcut
 [System.Collections.ArrayList]$result = New-Object -TypeName System.Collections.ArrayList
@@ -51,14 +51,14 @@ Get-ChildItem $app_path_arm64
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?=.exe)' }
 | Where-Object { IfGui -path $_.FullName }
 | ForEach-Object { $result.Add(@($_.Name, [System.IO.Path]::GetFileNameWithoutExtension($_.Name))) | Out-Null }
-$result | ConvertTo-Json > temp/ss_arm64_sc.json
+ConvertTo-Json $result > temp/ss_arm64_sc.json
 
 # arm64 bin
 [System.Collections.ArrayList]$result = New-Object -TypeName System.Collections.ArrayList
 Get-ChildItem $app_path_arm64
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?=.exe)' }
 | ForEach-Object { $result.Add(@($_.Name, [System.IO.Path]::GetFileNameWithoutExtension($_.Name))) | Out-Null }
-$result | ConvertTo-Json > temp/ss_arm64_bin.json
+ConvertTo-Json $result > temp/ss_arm64_bin.json
 
 # nano shortcut
 [System.Collections.ArrayList]$result = New-Object -TypeName System.Collections.ArrayList
@@ -66,11 +66,11 @@ Get-ChildItem $app_path_nano
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?=.exe)' }
 | Where-Object { IfGui -path $_.FullName }
 | ForEach-Object { $result.Add(@($_.Name, [System.IO.Path]::GetFileNameWithoutExtension($_.Name))) | Out-Null }
-$result | ConvertTo-Json > temp/ss_nano_sc.json
+ConvertTo-Json $result > temp/ss_nano_sc.json
 
 # nano bin
 [System.Collections.ArrayList]$result = New-Object -TypeName System.Collections.ArrayList
 Get-ChildItem $app_path_nano
 | Where-Object { $_.Name -match '[A-Za-z0-9]+(?=.exe)' }
 | ForEach-Object { $result.Add(@($_.Name, [System.IO.Path]::GetFileNameWithoutExtension($_.Name))) | Out-Null }
-$result | ConvertTo-Json > temp/ss_nano_bin.json
+ConvertTo-Json $result > temp/ss_nano_bin.json
